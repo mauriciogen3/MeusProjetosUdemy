1ba98cfc6599587d24a387edda41c28e3a0a6d48
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-09-16 20:03:31 -0300

    While e do While

---
+++ Controle/doWhile.js
@@ -0,0 +1,15 @@
+function getInteiroAleatorioEntre(min, max) {
+  const valor = Math.random() * (max - min) + min
+  return Math.floor(valor)
+}
+
+let opcao = 0
+
+do {
+  opcao = getInteiroAleatorioEntre(-1, 10)
+  console.log(`A opção escollllhida foi ${opcao}.`)
+} while (opcao != -1)
+console.log("Até a proxima!")
+
+
+
