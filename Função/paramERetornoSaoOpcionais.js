447066ddcdb1d02ab7458482c2e49a004104f726
Maurício Junior <mauriciogen3@hotmail.com>
2021-09-17 16:32:12 -0300

    area

---
+++ Função/paramERetornoSaoOpcionais.js
@@ -0,0 +1,7 @@
+function area(largura, altura){
+    const area = largura * alturaif(area>20){
+        console.log(`valor acima do permitido: ${area}m2.`)
+    } else{
+        return area
+    }
+}
