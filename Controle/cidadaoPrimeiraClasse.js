c6b50830076b3a94aa90331fa7939f6387a7596d
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-09-20 22:07:53 -0300

    Update cidadaoPrimeiraClasse.js

--- Controle/cidadaoPrimeiraClasse.js
+++ Controle/cidadaoPrimeiraClasse.js
@@ -1,1 +1,13 @@
 //quando a linguagem trata a funcao como um dado
+
+// higher-order function
+
+//criar de forma literal
+function fun1() {}
+
+//armazenar em uma variavel
+const fun2 = function() {}
+
+//armazenar em um array
+const array = [function (a,b){return a+b}, fun1, fun2]
+console.log(array[0](2,3))
