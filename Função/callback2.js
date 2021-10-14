0f01db82584024a465cdb044efd1f942f1819979
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-10-14 20:46:17 -0300

    Update callback2.js

--- Função/callback2.js
+++ Função/callback2.js
@@ -1,12 +1,31 @@
 const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
 
 //SEM CALLBACK
-let notasBaixas = []
+const notasBaixas1 = []
 //FOR IN  >< FOR OF
 for (let i in notas) {
   //PARA INSERIR AS NOTAS BAIXAS DENTRO DE UM ARRAY PRIMEIRO DEVE-SE CONSTRUIR UM ARRAY VAZIO
   if (notas[i] < 7) {
-    notasBaixas.push(notas[i])
+    // push adiciona um valor ao array
+    notasBaixas1.push(notas[i])
   }
 }
-console.log(notasBaixas)
+console.log(notasBaixas1)
+
+//-------------------------------------------------------------------------------
+// COM CALLBACK
+
+const notasBaixas2 = notas.filter(function (nota) {
+  return nota < 7
+})
+console.log(notasBaixas2)
+
+//---------------------OU----------------
+
+const notasBaixas3 = notas.filter(nota => nota < 7)
+console.log(notasBaixas3)
+
+//--------------------  OU -----------------
+const notasMenorQue7 = nota => nota < 8;
+const notasBaixas4 = notas.filter(notasMenorQue7)
+console.log(notasBaixas4)
