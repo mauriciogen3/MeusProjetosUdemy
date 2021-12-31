1c10d44b3e99049c3be9801878200b4276c98897
Maurício Junior <mauriciogen3@hotmail.com>
2021-12-31 10:23:51 -0300

    Create arrayMetodos.js

---
+++ Arrays/arrayMetodos.js
@@ -0,0 +1,14 @@
+const pilotos = ["hamilton", "massa", "Vetel", "Alonso", "Vestappen"]
+console.log(pilotos)
+
+pilotos.pop() //remove o ultimo elemento do array
+
+pilotos.push() //acrescenta um elemento na ultima posicao
+
+pilotos.shift()//remove o primeiro elemento do arrays
+
+pilotos.unshift("hamilton") //adiciona na primeira posicao
+
+pilotos.splice(2,0, "Botas", "Massa")
+
+console.log(pilotos)
